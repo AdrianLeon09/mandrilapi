dad3c3fc59f14117a5f2970aa40aae5fa5dfd39a
adria <leonadrian0909@gmail.com>
2025-06-03 12:13:25 -0300

    add database integration with EF

---
+++ Models/Service/MandrilContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+
+namespace MandrilAPI.Models.Service;
+
+public  class ContextDB : DbContext
+{
+    private string string2 = "texto prueba para crear rama 1"; 
+
+};
+  
+  
